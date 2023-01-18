ffda36bd7961ef574bfd88af060077e3a1ac83df
wotres <wotres@gmail.com>
2023-01-18 13:25:56 +0900

    [ADD] 환경 설정

---
+++ src/main/java/study/todospringboot/TodoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package study.todospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSpringbootApplication.class, args);
+	}
+
+}
